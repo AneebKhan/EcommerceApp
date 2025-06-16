9532fe674315ec895b922cb41aa48c12e0b3df1f
Aneeb Ahmed Khan <aneeb139@gmail.com>
2025-06-16 10:29:41 +0500

    Added On Boarding Screen

---
+++ lib/features/authentication/screens/onboarding/onboarding.dart
@@ -0,0 +1,49 @@
+import 'package:ecommerce_app/features/authentication/screens/onboarding/widgets/onboarding_page.dart';
+import 'package:ecommerce_app/features/authentication/screens/onboarding/widgets/onboarding_skip.dart';
+import 'package:ecommerce_app/utils/constants/image_strings.dart';
+import 'package:ecommerce_app/utils/constants/sizes.dart';
+import 'package:ecommerce_app/utils/device/device_utility.dart';
+import 'package:ecommerce_app/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../utils/constants/text_strings.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          /// Horizontal Scrollable Pages
+          PageView(
+            children: [
+              OnBoardingPage(
+                image: CustomImages.onBoardingImage1,
+                title: CustomTexts.onBoardingTitle1,
+                subTitle: CustomTexts.onBoardingSubTitle1,
+              ),
+              OnBoardingPage(
+                image: CustomImages.onBoardingImage2,
+                title: CustomTexts.onBoardingTitle2,
+                subTitle: CustomTexts.onBoardingSubTitle2,
+              ),
+              OnBoardingPage(
+                image: CustomImages.onBoardingImage3,
+                title: CustomTexts.onBoardingTitle3,
+                subTitle: CustomTexts.onBoardingSubTitle3,
+              ),
+            ],
+          ),
+
+          /// Skip Button
+          OnBoardingSkip(),
+
+          /// Dot Navigation SmoothPageIndicator
+          /// Circular Button
+        ],
+      ),
+    );
+  }
+}
