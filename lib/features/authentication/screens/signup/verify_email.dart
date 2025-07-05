22b3c94f47d860a819fb6a0272d3fea98a60a3e8
Aneeb Ahmed Khan <aneeb139@gmail.com>
2025-07-05 11:31:55 +0500

    Updated Verify Email Screen

--- lib/features/authentication/screens/signup/verify_email.dart
+++ lib/features/authentication/screens/signup/verify_email.dart
@@ -1,10 +1,12 @@
 import 'package:ecommerce_app/features/authentication/screens/login/login.dart';
 import 'package:ecommerce_app/utils/constants/sizes.dart';
+import 'package:ecommerce_app/utils/constants/text_strings.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../../utils/constants/image_strings.dart';
+import '../../../../utils/helpers/helper_functions.dart';
 
 class VerifyEmailScreen extends StatelessWidget {
   const VerifyEmailScreen({super.key});
@@ -18,16 +20,25 @@ class VerifyEmailScreen extends StatelessWidget {
           IconButton(onPressed: () => Get.off(() => LoginScreen()), icon: Icon(CupertinoIcons.clear))
         ],
       ),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(CustomSizes.defaultSpace),
           child: Column(
             children: [
               /// Image
-              Image(image: AssetImage(CustomImages.verifyEmailIllustration)),
+              Image(image: AssetImage(CustomImages.verifyEmailIllustration), width: CustomHelperFunctions.screenWidth() * 0.6),
+              const SizedBox(height: CustomSizes.spaceBtwSections),
 
               /// Title & Sub Title
+              Text(CustomTexts.confirmEmail, style: Theme.of(context).textTheme.headlineMedium, textAlign: TextAlign.center),
+              const SizedBox(height: CustomSizes.spaceBtwItems),
+              Text('support@aneebian.com', style: Theme.of(context).textTheme.labelLarge, textAlign: TextAlign.center),
+              const SizedBox(height: CustomSizes.spaceBtwItems),
+              Text(CustomTexts.confirmEmailSubtitle, style: Theme.of(context).textTheme.labelMedium, textAlign: TextAlign.center),
+              const SizedBox(height: CustomSizes.spaceBtwItems),
+              
               /// Buttons
+
             ],
           ),
         ),
