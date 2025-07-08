061c61a723c4961622a38e39ad8966728d9a7cfe
Aneeb Ahmed Khan <aneeb139@gmail.com>
2025-07-08 10:49:50 +0500

    Added  Password Screen

--- lib/features/authentication/screens/password_configuration/reset_password.dart
+++ lib/features/authentication/screens/password_configuration/reset_password.dart
@@ -1,10 +1,37 @@
+import 'package:ecommerce_app/utils/constants/sizes.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ResetPasswordScreen extends StatelessWidget {
   const ResetPasswordScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(onPressed: () => Get.back(), icon: const Icon(CupertinoIcons.clear))
+        ],
+      ),
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(CustomSizes.defaultSpace),
+          child: Column(
+            children: [
+              /// Image with 60% of screen width
+
+
+              /// Title & Sub Title
+
+
+              /// Buttons
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
