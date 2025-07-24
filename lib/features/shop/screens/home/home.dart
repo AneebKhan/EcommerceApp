cdd96be1eda42388826bc2a401e640c0a2903211
Aneeb Ahmed Khan <aneeb139@gmail.com>
2025-07-24 11:17:54 +0500

    Updated Home Screen

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,8 +6,64 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: CustomColors.primary,
+              padding: EdgeInsets.all(0),
+              child: Stack(
+                children: [
+                  CustomCircularContainer(),
+                  Container(
+                    width: 400,
+                    height: 400,
+                    padding: EdgeInsets.all(0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(400),
+                      color: CustomColors.textWhite.withOpacity(0.1),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
 
+class CustomCircularContainer extends StatelessWidget {
+  const CustomCircularContainer({
+    super.key,
+    this.child,
+    this.width = 400,
+    this.height = 400,
+    this.radius = 400,
+    this.padding = 0,
+    this.backgroundColor = CustomColors.textWhite,
+  });
+
+  final double? width;
+  final double? height;
+  final double radius;
+  final double padding;
+  final Widget? child;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 400,
+      height: 400,
+      padding: EdgeInsets.all(0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(400),
+        color: CustomColors.textWhite.withOpacity(0.1),
+      ),
+      child: child,
     );
   }
 }
